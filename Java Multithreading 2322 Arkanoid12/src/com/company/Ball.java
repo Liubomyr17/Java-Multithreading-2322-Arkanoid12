0e0355f7b39141cc09a1d8a256ec2a871096cc3a
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-07-30 18:57:26 +0300

    Add files via upload

---
+++ Java Multithreading 2322 Arkanoid12/src/com/company/Ball.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Ball extends BaseObject {
+    public Ball(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
+    @Override
+    void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    void move() {
+
+    }
+}
